e0b4dc3f7ac37b541967b021812eabd5f4a05e5a
Tommy Yang <121709432+chuanqi-yang@users.noreply.github.com>
2026-01-05 10:36:57 -0800

    Update security-log.js

--- security-log.js
+++ security-log.js
@@ -1,9 +1,5 @@
 // security-log.js
-// security-log.js
-
-(() => {
-  fetch("steep-forest-3ea6.0512tommy.workers.dev", {
-    method: "POST",
-    mode: "no-cors"
-  });
-})();
+fetch("https://steep-forest-3ea6.0512tommy.workers.dev", {
+  method: "POST",
+  mode: "no-cors"
+});
